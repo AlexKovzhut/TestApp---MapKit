3e43f2818bd2226ff44c314485cbdb03cf3f0d13
Alexander Kovzhut <akovzhut@icloud.com>
2021-10-09 20:57:55 +0300

    Initial Commit

---
+++ TestApp - MapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp - MapKit
+//
+//  Created by Alexander Kovzhut on 09.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
