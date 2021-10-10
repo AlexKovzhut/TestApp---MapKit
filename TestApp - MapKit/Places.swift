80d4a2bd62fdce321723cc97f62cf9c8ebea7b0d
Alexander Kovzhut <akovzhut@icloud.com>
2021-10-10 19:36:45 +0300

    Commit

---
+++ TestApp - MapKit/Places.swift
@@ -0,0 +1,29 @@
+//
+//  Places.swift
+//  TestApp - MapKit
+//
+//  Created by Alexander Kovzhut on 10.10.2021.
+//
+
+import Foundation
+import MapKit
+
+class Places: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String?
+    let category: String?
+    let coordinate: CLLocationCoordinate2D
+    
+    init (title: String?, location: String?, category: String?, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = location
+        self.category = category
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
+}
--- TestApp - MapKit/ViewController.swift
+++ TestApp - MapKit/ViewController.swift
@@ -14,9 +14,39 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let initialLocation = CLLocation(latitude: 55.751244, longitude: 37.618423)
+        
+        MapView.centerLocation(initialLocation)
+        
+        let cameraCenter = CLLocation(latitude: 55.751244, longitude: 37.618423)
+        let region = MKCoordinateRegion(center: cameraCenter.coordinate, latitudinalMeters: 5000, longitudinalMeters: 5000)
+        MapView.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
+        
+        let zoomRage = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 25_000)
+        MapView.setCameraZoomRange(zoomRage, animated: true)
+        
+        let place = Places(
+            title: "Red Square",
+            location: "Moscow",
+            category: "Squares",
+            coordinate: CLLocationCoordinate2D(
+                latitude: 55.754093,
+                longitude: 37.620407
+            )
+        )
+        
+        MapView.addAnnotation(place)
     }
-
-
 }
 
+extension MKMapView {
+    // метод определяющий локацию в радиусе 1 км
+    func centerLocation(_ location: CLLocation, regionRadius: CLLocationDistance = 500) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        
+        setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension ViewController: MPKIt
